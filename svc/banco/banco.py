72318b6983463d97d65cb4e9bfdf9a26125d3d3e
alunoufc <pjmatos87@gmail.com>
2022-05-02 09:14:25 -0300

    implemtação no github

---
+++ svc/banco/banco.py
@@ -0,0 +1,40 @@
+class Banco:
+    def __init__(self):
+        self.contas = [None] * 100
+        self.indice = 0
+
+    def cadastrar(self, conta):
+        self.contas[self.indice] = conta
+        self.indice += 1
+
+    def procurar_conta(self,numero):
+        achou = False
+        while achou is False and i < self.indice:
+            if self.contas[i].get_numero() == numero:
+                achou = True
+            else:
+                i += 1
+            if achou is True:
+                return self.contas[i]
+            else:
+                return None
+
+    def creditar(self,numero,valor):
+        pass
+
+    def debitar(self,numero,valor):
+        conta = self.procurar_conta(numero)
+        if conta:
+            conta.debitar(valor)
+        else:
+            print('Conta Inexistente!')
+
+    def saldo(self, numero):
+        pass
+
+    def transferir(self, origem, destino, valor):
+        pass
+
+
+def banco():
+    return None
---
+++ svc/banco/criar_banco.py
@@ -0,0 +1,6 @@
+from banco import banco
+
+
+class CriarBanco:
+    if __name__ == '__main__':
+
